1a40bc95765198df566818b054c94a84688dfd6b
dlaehdwn1338 <dlaehdwn1338@gmail.com>
2024-01-19 11:24:15 +0900

    Initial commit

---
+++ src/main/java/net/dap195dapa/copyresources/CopyResourcesClient.java
@@ -0,0 +1,10 @@
+package net.dap195dapa.copyresources;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopyResourcesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dap195dapa/copyresources/CopyResourcesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dap195dapa.copyresources;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopyResourcesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
